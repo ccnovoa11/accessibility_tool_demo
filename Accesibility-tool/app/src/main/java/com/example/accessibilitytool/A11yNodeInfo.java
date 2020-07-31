98ac14de48f0227748ab6ab9e50f17431fc5fa82
Cristian Novoa <cc.novoa11@uniandes.edu.co>
2020-07-31 18:46:43 -0500

    create file and some stadistics

---
+++ Accesibility-tool/app/src/main/java/com/example/accessibilitytool/A11yNodeInfo.java
@@ -0,0 +1,317 @@
+package com.example.accessibilitytool;
+
+import android.graphics.Rect;
+import android.os.Build.VERSION;
+import androidx.viewpager.widget.ViewPager ;
+import androidx.core.view.accessibility.AccessibilityNodeInfoCompat;
+import androidx.core.view.accessibility.AccessibilityNodeInfoCompat.AccessibilityActionCompat;
+import android.view.View;
+import android.view.accessibility.AccessibilityNodeInfo;
+import android.view.accessibility.AccessibilityNodeInfo.AccessibilityAction;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Switch;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+
+public class A11yNodeInfo implements Iterable<A11yNodeInfo>, Comparator<A11yNodeInfo> {
+    private static final ArrayList<Class<? extends View>> ACTIVE_CLASSES = new ArrayList();
+    private final AccessibilityNodeInfoCompat mNodeInfo;
+
+    public static A11yNodeInfo wrap(AccessibilityNodeInfo node) {
+        return node == null ? null : new A11yNodeInfo(node);
+    }
+
+    public static A11yNodeInfo wrap(AccessibilityNodeInfoCompat node) {
+        return node == null ? null : new A11yNodeInfo(node);
+    }
+
+    protected A11yNodeInfo() {
+        this.mNodeInfo = null;
+    }
+
+    protected A11yNodeInfo(AccessibilityNodeInfo nodeInfo) {
+        this(new AccessibilityNodeInfoCompat(nodeInfo));
+    }
+
+    protected A11yNodeInfo(AccessibilityNodeInfoCompat nodeInfoCompat) {
+        if (nodeInfoCompat == null) {
+            throw new RuntimeException("Wrapping a null node doesn't make sense");
+        } else {
+            this.mNodeInfo = nodeInfoCompat;
+        }
+    }
+
+    public int compare(A11yNodeInfo lhs, A11yNodeInfo rhs) {
+        int result = lhs.getSpeakableText().compareTo(rhs.getSpeakableText());
+        if (result != 0) {
+            return result;
+        } else {
+            Rect lhsRect = lhs.getBoundsInScreen();
+            Rect rhsRect = rhs.getBoundsInScreen();
+            if (result != 0) {
+                return result;
+            } else if (lhsRect.top < rhsRect.top) {
+                return -1;
+            } else if (lhsRect.top > rhsRect.top) {
+                return 1;
+            } else if (lhsRect.left < rhsRect.left) {
+                return -1;
+            } else if (lhsRect.left > rhsRect.left) {
+                return 1;
+            } else if (lhsRect.right < rhsRect.right) {
+                return -1;
+            } else if (lhsRect.right > rhsRect.right) {
+                return 1;
+            } else if (lhsRect.bottom < rhsRect.bottom) {
+                return -1;
+            } else if (lhsRect.bottom > rhsRect.bottom) {
+                return 1;
+            } else {
+                return result != 0 ? result : 0;
+            }
+        }
+    }
+
+    public List<AccessibilityActionCompat> getActionList() {
+        return this.mNodeInfo.getActionList();
+    }
+
+    public int getActions() {
+        return this.mNodeInfo.getActions();
+    }
+
+    public boolean isActiveElement() {
+        Iterator var1 = ACTIVE_CLASSES.iterator();
+
+        while(var1.hasNext()) {
+            Class<? extends View> clazz = (Class)var1.next();
+            if (this.getClassName().equalsIgnoreCase(clazz.getName())) {
+                return true;
+            }
+        }
+
+        if (VERSION.SDK_INT >= 21) {
+            if (this.getActionList().contains(AccessibilityAction.ACTION_CLICK)) {
+                return true;
+            }
+
+            if (VERSION.SDK_INT >= 23 && this.getActionList().contains(AccessibilityAction.ACTION_CONTEXT_CLICK)) {
+                return true;
+            }
+
+            if (this.getActionList().contains(AccessibilityAction.ACTION_LONG_CLICK)) {
+                return true;
+            }
+
+            if (this.getActionList().contains(AccessibilityAction.ACTION_SELECT)) {
+                return true;
+            }
+        }
+
+        int actions = this.getActions();
+        return (actions & 16) != 0 || (actions & 32) != 0 || (actions & 4) != 0;
+    }
+
+    public boolean performAction(A11yNodeInfo.Actions action) {
+        return this.mNodeInfo.performAction(action.getAndroidValue());
+    }
+
+    public AccessibilityNodeInfoCompat getAccessibilityNodeInfoCompat() {
+        return this.mNodeInfo;
+    }
+
+    public Rect getBoundsInScreen() {
+        Rect result = new Rect();
+        this.mNodeInfo.getBoundsInScreen(result);
+        return result;
+    }
+
+    public A11yNodeInfo getChild(int i) {
+        if (i >= this.mNodeInfo.getChildCount()) {
+            throw new IndexOutOfBoundsException();
+        } else {
+            return new A11yNodeInfo(this.mNodeInfo.getChild(i));
+        }
+    }
+
+    public int getChildCount() {
+        return this.mNodeInfo.getChildCount();
+    }
+
+    public String getClassName() {
+        return this.mNodeInfo.getClassName().toString();
+    }
+
+    public CharSequence getContentDescription() {
+        return this.mNodeInfo.getContentDescription();
+    }
+
+    public String getContentDescriptionAsString() {
+        return this.mNodeInfo.getContentDescription() == null ? "" : this.mNodeInfo.getContentDescription().toString();
+    }
+
+    public int getDepthInTree() {
+        int result = 0;
+
+        for(A11yNodeInfo parentNode = this.getParent(); parentNode != null; ++result) {
+            parentNode = parentNode.getParent();
+        }
+
+        return result;
+    }
+
+    public A11yNodeInfo getLabeledBy() {
+        return wrap(this.mNodeInfo.getLabeledBy());
+    }
+
+    public A11yNodeInfo getParent() {
+        return this.mNodeInfo.getParent() == null ? null : new A11yNodeInfo(this.mNodeInfo.getParent());
+    }
+
+    public String getSpeakableText() {
+        if (this.getContentDescription() != null) {
+            return this.getContentDescriptionAsString();
+        } else {
+            return this.getText() != null ? this.getTextAsString() : "";
+        }
+    }
+
+    public CharSequence getText() {
+        return this.mNodeInfo.getText();
+    }
+
+    public String getTextAsString() {
+        return this.getText() != null ? this.getText().toString() : "";
+    }
+
+    public String getViewIdResourceName() {
+        return this.mNodeInfo.getViewIdResourceName() == null ? "" : this.mNodeInfo.getViewIdResourceName();
+    }
+
+    public Iterator<A11yNodeInfo> iterator() {
+        return new Iterator<A11yNodeInfo>() {
+            private int mNextIndex = 0;
+
+            public boolean hasNext() {
+                return this.mNextIndex < A11yNodeInfo.this.getChildCount() && (A11yNodeInfo.this.mNodeInfo == null || A11yNodeInfo.this.mNodeInfo.getChild(this.mNextIndex) != null);
+            }
+
+            public A11yNodeInfo next() {
+                return A11yNodeInfo.this.getChild(this.mNextIndex++);
+            }
+
+            public void remove() {
+            }
+        };
+    }
+
+    public String toViewHeirarchy() {
+        final StringBuilder result = new StringBuilder();
+        result.append("--------------- Accessibility Node Hierarchy ---------------\n");
+        this.visitNodes(new A11yNodeInfo.OnVisitListener() {
+            public boolean onVisit(A11yNodeInfo nodeInfo) {
+                result.append(nodeInfo.getClassName());
+                result.append(' ');
+                result.append(nodeInfo.getContentDescription());
+                result.append('\n');
+                return false;
+            }
+        });
+        result.append("--------------- Accessibility Node Hierarchy ---------------");
+        return result.toString();
+    }
+
+
+    public boolean isClassType(Class<?> clazz) {
+        return clazz.getName().equalsIgnoreCase(this.getClassName());
+    }
+
+    public boolean isScrollable() {
+        return this.mNodeInfo.isScrollable();
+    }
+
+    public boolean isVisibleToUser() {
+        return this.mNodeInfo.isVisibleToUser();
+    }
+
+    public boolean isInVisibleScrollableField() {
+        A11yNodeInfo tempNode = wrap(this.mNodeInfo);
+
+        A11yNodeInfo scrollableView;
+        for(scrollableView = null; tempNode.getParent() != null; tempNode = tempNode.getParent()) {
+            if (tempNode.isScrollable() && !tempNode.isClassType(ViewPager.class)) {
+                scrollableView = tempNode;
+            }
+        }
+
+        return scrollableView != null && scrollableView.isVisibleToUser();
+    }
+
+    public String toString() {
+        if (this.mNodeInfo == null) {
+            throw new RuntimeException("This shouldn't be null");
+        } else {
+            return this.mNodeInfo.toString();
+        }
+    }
+
+    public A11yNodeInfo visitNodes(A11yNodeInfo.OnVisitListener onVisitListener) {
+        if (onVisitListener.onVisit(this)) {
+            return this;
+        } else {
+            Iterator var2 = this.iterator();
+
+            A11yNodeInfo result;
+            do {
+                if (!var2.hasNext()) {
+                    return null;
+                }
+
+                A11yNodeInfo child = (A11yNodeInfo)var2.next();
+                result = child.visitNodes(onVisitListener);
+            } while(result == null);
+
+            return result;
+        }
+    }
+
+    static {
+        ACTIVE_CLASSES.add(Button.class);
+        ACTIVE_CLASSES.add(Switch.class);
+        ACTIVE_CLASSES.add(CheckBox.class);
+        ACTIVE_CLASSES.add(EditText.class);
+    }
+
+    public interface OnVisitListener {
+        boolean onVisit(A11yNodeInfo var1);
+    }
+
+    public static enum Actions {
+        ACCESSIBILITY_FOCUS(64),
+        CLEAR_ACCESSIBILITY_FOCUS(128),
+        CLEAR_FOCUS(2),
+        CLEAR_SELECTION(8),
+        CLICK(16),
+        COLLAPSE(524288),
+        COPY(16384),
+        CUT(65536),
+        LONG_CLICK(32),
+        PASTE(32768),
+        PREVIOUS_AT_MOVEMENT_GRANULARITY(512),
+        PREVIOUS_HTML_ELEMENT(2048);
+
+        private final int mAndroidValue;
+
+        private Actions(int androidValue) {
+            this.mAndroidValue = androidValue;
+        }
+
+        int getAndroidValue() {
+            return this.mAndroidValue;
+        }
+    }
+}
--- Accesibility-tool/app/src/main/java/com/example/accessibilitytool/AccessibilityToolService.java
+++ Accesibility-tool/app/src/main/java/com/example/accessibilitytool/AccessibilityToolService.java
@@ -3,6 +3,7 @@ package com.example.accessibilitytool;
 import android.accessibilityservice.AccessibilityService;
 import android.graphics.PixelFormat;
 import android.os.Build;
+import android.os.Environment;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -14,18 +15,27 @@ import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
+import androidx.core.view.accessibility.AccessibilityNodeInfoCompat;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 //import androidx.test.espresso.contrib.AccessibilityChecks;
 
 public class AccessibilityToolService extends AccessibilityService {
     FrameLayout mLayout;
+    FileOutputStream fOut;
+    OutputStreamWriter osw;
+    Boolean writeReport = false;
 
+    static final String HTML = new String("<html>");
     @Override
     protected void onServiceConnected() {
         // Create an overlay and display the action bar
@@ -45,18 +55,20 @@ public class AccessibilityToolService extends AccessibilityService {
         configureStopButton();
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        Log.d("WARDIOLA", event.toString());
+//        Log.d("WARDIOLA", event.toString());
+        if(writeReport){
+            switch (event.getEventType()){
+                case AccessibilityEvent.TYPE_VIEW_CLICKED:
+                    try{
+                        writeInFile("<p>"+event.getSource()+"</p>");
+                        Log.d("WARDIOLA2", A11yNodeInfo.wrap(getRootInActiveWindow()).toViewHeirarchy());
 
-        switch (event.getEventType()){
-            case AccessibilityEvent.TYPE_VIEW_CLICKED:
-                try{
-                    Log.d("WARDIOLA2", String.valueOf(event.getSource()));
-                }catch (Exception e){
-                    Log.d("WARDIOLA2", e.getMessage());
-                }
+                    }catch (Exception e){
+                        Log.d("WARDIOLA2", e.getMessage());
+                    }
+            }
         }
     }
 
@@ -70,14 +82,37 @@ public class AccessibilityToolService extends AccessibilityService {
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Commands commands = new Commands();
-                try {
-                    commands.showDevicesWithADB();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+
+                writeReport = true;
+                writeInFile("<html><body><h2>ACCESSIBILITY REPORT</h2>");
+
+//                try {
+//                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+//                    // catches IOException below
+////                    BufferedWriter bw = new BufferedWriter(new FileWriter("report.html"));
+////                    bw.write("<html><head><title>New Page</title></head><body><p>This is Body</p></body></html>");
+////                    bw.close();
+//
+//                    /* We have to use the openFileOutput()-method
+//                     * the ActivityContext provides, to
+//                     * protect your file from others and
+//                     * This is done for security-reasons.
+//                     * We chose MODE_WORLD_READABLE, because
+//                     *  we have nothing to hide in our file */
+//                    fOut = new FileOutputStream(new File(getExternalFilesDir(null),"report.html"), true);
+//                    osw = new OutputStreamWriter(fOut);
+//                    OutputStreamWriter osw = new OutputStreamWriter(fOut);
+//                    // Write the string to the file
+//                    osw.write("<html><body><h1>ACCESSIBILITY REPORT</h1>");
+//
+//                    /* ensure that everything is
+//                     * really written out and close */
+//                    osw.flush();
+//                    osw.close();
+//
+//                } catch (IOException ioe) {
+//                    ioe.printStackTrace();
+//                }
 
                 Toast toast = Toast.makeText(getApplicationContext(),
                         "Start button",
@@ -92,11 +127,43 @@ public class AccessibilityToolService extends AccessibilityService {
         stopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                writeReport = false;
+                writeInFile("</body></html>");
                 Toast toast = Toast.makeText(getApplicationContext(),
                         "Stop button",
                         Toast.LENGTH_SHORT);
                 toast.show();
             }
         });
+    }
+
+    public void writeInFile(String append){
+        try {
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+            // catches IOException below
+//                    BufferedWriter bw = new BufferedWriter(new FileWriter("report.html"));
+//                    bw.write("<html><head><title>New Page</title></head><body><p>This is Body</p></body></html>");
+//                    bw.close();
+
+            /* We have to use the openFileOutput()-method
+             * the ActivityContext provides, to
+             * protect your file from others and
+             * This is done for security-reasons.
+             * We chose MODE_WORLD_READABLE, because
+             *  we have nothing to hide in our file */
+            fOut = new FileOutputStream(new File(getExternalFilesDir(null),"report.html"), true);
+            osw = new OutputStreamWriter(fOut);
+            OutputStreamWriter osw = new OutputStreamWriter(fOut);
+            // Write the string to the file
+            osw.write(append);
+
+            /* ensure that everything is
+             * really written out and close */
+            osw.flush();
+            osw.close();
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
     }
 }
